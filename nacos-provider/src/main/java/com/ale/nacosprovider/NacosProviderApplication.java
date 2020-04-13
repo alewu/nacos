4166df43a45a52dc1a37dcf95e6baf21e17cef38
alewu <wywujiawei@163.com>
2020-04-13 23:12:54 +0800

    first commit

---
+++ nacos-provider/src/main/java/com/ale/nacosprovider/NacosProviderApplication.java
@@ -0,0 +1,18 @@
+package com.ale.nacosprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+
+}
